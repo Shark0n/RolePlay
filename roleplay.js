8de3be14be38598a8e2f30fa3390aadfb94c3355
Alex Kolundaev <kolundaev@ancort.ru>
2015-02-10 20:16:53 +0300

    1.3
    2015.02.10 Добавлены функции автоматической подгонки и центрирования изображения под заданные размеры аватарок: avatarHeight х avatarWidth

--- roleplay.js
+++ roleplay.js
@@ -12,12 +12,13 @@
 // 1.0 2015.02.10 Рабочая версия скрипта для одной игры
 // 1.1 2015.02.10 Переделан для использования одного скрипта для нескольких игр. Добавлены комментарии по коду.
 // 1.2 2015.02.10 Повышено удобство инициализации скрипта, добавлены дополнительные настройки для игровых аватарок: высота и ширина
+// 1.3 2015.02.10 Добавлены функции автоматической подгонки и центрирования изображения под заданные размеры аватарок: avatarHeight х avatarWidth
 
 jQuery(document).ready(function () {
-    // Выполняем шаги 1 и 2. Затем копируем скрипт roleplay.1.2.js на хостинг в корневую папку форума или папку скриптов js для форума <локальный путь до скрипта на хостинге>/.
+    // Выполняем шаги 1 и 2. Затем копируем скрипт roleplay.js на хостинг в корневую папку форума или папку скриптов js для форума <локальный путь до скрипта на хостинге>/.
     // Прописываем в <header> в index.php форума следующую строку без "//"
 
-    // <script src="<локальный путь до скрипта на хостинге>/roleplay.1.2.js"></script>
+    // <script src="<локальный путь до скрипта на хостинге>/roleplay.js"></script>
     
 	// Можно описать несколько игр сразу или добавлять по мере необходимости.
     // Скрипт сам определяет по названию темы - нужно ли ему выполняться или нет.
@@ -64,10 +65,15 @@ jQuery(document).ready(function () {
 
 	function replaceGamer(nameAObj, parentObj, gameIndex) {
 		//Создаем игровую аватарку
-		var newNameAObj = nameAObj.attr("title", nameAObj.html()).clone().html('<img alt="' + nameAObj.html() +  '" src="'+ gamesArr[gameIndex][nameAObj.html()].fakeAvatar +'"><br/>').prependTo(parentObj);
+		var newNameAObj = nameAObj.attr("title", nameAObj.html())
+      .clone()
+      .html('<div style="overflow: hidden; width:' + avatarWidth + 'px; height:' + avatarHeight + 'px;">' +
+            '<img alt="' + nameAObj.html() +  '" src="'+ gamesArr[gameIndex][nameAObj.html()].fakeAvatar +'"></img>' +
+            '</div><br/>')
+      .prependTo(parentObj);
 		
 		// берем картинку
-		var $img = newNameAObj.children("img");
+		var $img = newNameAObj.children("div").children("img");
 		var newHeight;
 		var newWidth;
 		var shiftTop;
@@ -77,36 +83,28 @@ jQuery(document).ready(function () {
 		var height = $img.height();
     var pow;
     
-		console.log(width + 'x' + height);
 		if ((height/avatarHeight) > (width/avatarWidth)) {
-      console.log("h > w");
       if (avatarHeight - height > 0) { pow = 1 } 
       else { pow = -1 }
       newWidth = avatarWidth;
-			newHeight = avatarHeight * Math.pow(width/avatarWidth, pow);
+			newHeight = height * Math.pow(width/avatarWidth, pow);
 			shiftLeft = 0;
 			shiftTop = - (newHeight - avatarHeight) / 2;
 		}
 		else {
-      console.log("w > h");
       if (avatarWidth - width > 0) { pow = 1 } 
       else { pow = -1 }
 			newHeight = avatarHeight;
-			newWidth = avatarWidth *  Math.pow(height/avatarHeight, pow);
-      console.log(height);
-      console.log(newHeight);
-      console.log(height/avatarHeight);
-      console.log(avatarWidth - width);
-      console.log((avatarWidth - width) % 2);
-      console.log(Math.pow(height/avatarHeight, (avatarWidth - width) % 2));
-      console.log(avatarWidth);
-      console.log(newWidth);
+			newWidth = width * Math.pow(height/avatarHeight, pow);
 			shiftTop = 0;
 			shiftLeft = - (newWidth - avatarWidth) / 2;
 		}
 		
-		console.log(newHeight + ', ' + newWidth + ', ' + shiftTop + ', ' + shiftLeft);
-		 
+		//console.log(newHeight + ', ' + newWidth + ', ' + shiftTop + ', ' + shiftLeft);
+		$img.attr("height", newHeight)
+      .attr("width", newWidth)
+      .attr("style", 'margin-top: '+ shiftTop +'px; margin-left: '+ shiftLeft +'px;');
+    
 		//Изменяем имя пользователя
 		nameAObj.html(gamesArr[gameIndex][nameAObj.html()].fakeNickname);
 	}
