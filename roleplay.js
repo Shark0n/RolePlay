d2502f029d5912fa5ebb488c485ce8fa629cdcea
Alex Kolundaev <kolundaev@ancort.ru>
2015-02-11 12:41:37 +0300

    Refactoring
    Иснтрукция по установке вынесена в репозиторий https://github.com/Shark0n/RolePlay

--- roleplay.js
+++ roleplay.js
@@ -1,40 +1,18 @@
-// Скрипт RolePlay
-// Предназначен для использования в форумных играх 5druzey.ru/forum
-// Скрипт заменяет аватарки и имена игроков форумной ролевой игры только в нужных ветках форума.
-//
-// Автор: Shark0n callups@gmail.com
-//
-// Требования:
-// движок форума phpbb 3.1.х
-// jQuery 1.5.х или выше
-//
-// История версий:
-// 1.0 2015.02.10 Рабочая версия скрипта для одной игры
-// 1.1 2015.02.10 Переделан для использования одного скрипта для нескольких игр. Добавлены комментарии по коду.
-// 1.2 2015.02.10 Повышено удобство инициализации скрипта, добавлены дополнительные настройки для игровых аватарок: высота и ширина
-// 1.3 2015.02.10 Добавлены функции автоматической подгонки и центрирования изображения под заданные размеры аватарок: avatarHeight х avatarWidth
+// RolePlay - is jQuery script for better user experience forum role playing!
+// HOW TO INSTALL see on https://github.com/Shark0n/RolePlay
 
 jQuery(document).ready(function () {
-    // Выполняем шаги 1 и 2. Затем копируем скрипт roleplay.js на хостинг в корневую папку форума или папку скриптов js для форума <локальный путь до скрипта на хостинге>/.
-    // Прописываем в <header> в index.php форума следующую строку без "//"
-
-    // <script src="<локальный путь до скрипта на хостинге>/roleplay.js"></script>
-    
-	// Можно описать несколько игр сразу или добавлять по мере необходимости.
-    // Скрипт сам определяет по названию темы - нужно ли ему выполняться или нет.
-	//###########################################################################
-    
-	// Можно задать свою высоту и ширину для всех игровых аватарок в пикселях
-	var avatarHeight = '128';
-	var avatarWidth = '128';
+	// height and width - you want to avatar
+	var avatarHeight = '128'; // pixels
+	var avatarWidth  = '128'; // pixels
 	
     var gamesArr = [];
 	var gametitle = '';
 	
-	//1. Указываем название существующей игры №1
+	// Game title in forum thread
 	gametitle = 'Случай в тихом океане';
 	gamesArr[gametitle] = [];
-	//2. Описываем существующих игроков
+	// Gamers: real name, fake name and fake avatar
 	gamesArr[gametitle]['Shark0n'] = {
 		'fakeNickname':'Бенджамин Тод',
 		'fakeAvatar':'http://webnewsmedia.net/wp-content/uploads/2012/12/Detective-903x1024.jpg'
@@ -53,7 +31,7 @@ jQuery(document).ready(function () {
       };
 
 	/*
-	// Повторяем тоже самое для следующей игры
+	// You may add same information for another game
 	gametitle = '<Название темы>';
 	gamesArr[gametitle]['<Ник игрока>'] = {
         'fakeNickname':'<игровое имя>',
@@ -61,10 +39,10 @@ jQuery(document).ready(function () {
       };
     */
 	  
-    //3. Готово! Остальное, скрипт сделает сам
+    // Ready
 
 	function replaceGamer(nameAObj, parentObj, gameIndex) {
-		//Создаем игровую аватарку
+		// Make fake avatar
 		var newNameAObj = nameAObj.attr("title", nameAObj.html())
       .clone()
       .html('<div style="overflow: hidden; width:' + avatarWidth + 'px; height:' + avatarHeight + 'px;">' +
@@ -72,7 +50,6 @@ jQuery(document).ready(function () {
             '</div><br/>')
       .prependTo(parentObj);
 		
-		// берем картинку
 		var $img = newNameAObj.children("div").children("img");
 		var newHeight;
 		var newWidth;
@@ -100,12 +77,11 @@ jQuery(document).ready(function () {
 			shiftLeft = - (newWidth - avatarWidth) / 2;
 		}
 		
-		//console.log(newHeight + ', ' + newWidth + ', ' + shiftTop + ', ' + shiftLeft);
 		$img.attr("height", newHeight)
-      .attr("width", newWidth)
-      .attr("style", 'margin-top: '+ shiftTop +'px; margin-left: '+ shiftLeft +'px;');
+			.attr("width", newWidth)
+			.attr("style", 'margin-top: '+ shiftTop +'px; margin-left: '+ shiftLeft +'px;');
     
-		//Изменяем имя пользователя
+		// Change name
 		nameAObj.html(gamesArr[gameIndex][nameAObj.html()].fakeNickname);
 	}
   
@@ -114,14 +90,14 @@ jQuery(document).ready(function () {
 	if ($.inArray(currentTitle, gamesArr)) {
 		$('dl.postprofile dt').each(function (index) {
 			if ($(this).children('a').size() == 1) {
-				//У пользователя нет аватар, но нам надо изменить имя и вставить свою аватарку
+				// User have only real name - adding avatar and changing name
 				var nicknameAObj = $(this).children('a:eq(0)');
 				if ($.inArray(nicknameAObj.html(), gamesArr[currentTitle])) {
 					replaceGamer(nicknameAObj, $(this), currentTitle);
 				}
 			}
 			else if ($(this).children('a').size() == 2) {
-				//У пользователя есть своя аватар, нам надо изменить имя и аватарку
+				// User have both real name and his avatar - changing avatar and name
 				var nicknameAObj = $(this).children('a:eq(1)');
 				var avatarAObj = $(this).children('a:eq(0)');
 				if ($.inArray(nicknameAObj.html(), gamesArr[currentTitle])) {
