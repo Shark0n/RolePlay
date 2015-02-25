9985a942cafc2592164599352327d727bb465a6d
Alex Kolundaev <kolundaev@ancort.ru>
2015-02-25 11:15:14 +0300

    1.7 2015.02.25
    - Математические функции для вычисления ширины и высоты заменены на CSS width:auto, height:auto
    - Добавлен roleplay.html-файл для отладки и тестирования скрипта на jsFiddle http://jsfiddle.net/Shark0n/wrc1hoaL/42/

--- roleplay.js
+++ roleplay.js
@@ -1,8 +1,7 @@
 // RolePlay - is jQuery script for better user experience forum role playing!
 // HOW TO INSTALL see on https://github.com/Shark0n/RolePlay
 
-jQuery(document).ready(function() 
-{
+jQuery(document).ready(function() {
 	// height and width - you want to avatar
 	var avatarHeight = '128'; // pixels
 	var avatarWidth = '128'; // pixels
@@ -15,32 +14,31 @@ jQuery(document).ready(function()
 	gametitle = 'Случай в тихом океане';
 	gamesArr[gametitle] = [];
 	// Gamers: real name, fake name and fake avatar
-	gamesArr[gametitle]['Shark0n'] = 
-	{
+	gamesArr[gametitle]['Shark0n'] = {
 		'fakeNickname': 'Бенджамин Тод',
 		'fakeAvatar': 'http://webnewsmedia.net/wp-content/uploads/2012/12/Detective-903x1024.jpg', //903x1024
 		'fakeAvatarObj': 'empty'
 	};
-	gamesArr[gametitle]['Starc'] = 
-	{
+	gamesArr[gametitle]['Starc'] = {
 		'fakeNickname': 'Элиос Мур',
 		'fakeAvatar': 'http://rewalls.com/images/201012/reWalls.com_12147.jpg',
 		'fakeAvatarObj': 'empty'
 	};
-	gamesArr[gametitle]['Фрайзен'] = 
-	{
+	gamesArr[gametitle]['Фрайзен'] = {
 		'fakeNickname': 'Стив Лоуренс',
 		'fakeAvatar': 'http://img2.1001golos.ru/ratings/73000/72387/pic1.jpg',
 		'fakeAvatarObj': 'empty'
 	};
-	gamesArr[gametitle]['Gellert Grindelwald'] = 
-	{
+	gamesArr[gametitle]['Gellert Grindelwald'] = {
 		'fakeNickname': 'Эмили Джонс',
 		'fakeAvatar': 'http://static.kinokopilka.tv/system/images/photos/images/000/128/772/128772_large.jpg',
 		'fakeAvatarObj': 'empty'
 	};
 	// {STOP GAME DESCRIPTION}
 	// Ready
+
+
+	// Declare functions
 
 	function replaceGamer(nameAObj, parentObj, gameIndex) 
 	{
@@ -51,8 +49,8 @@ jQuery(document).ready(function()
 			// Make fake avatar
 			var newNameAObj = nameAObj.attr("title", nameAObj.html())
 				.clone()
-				.html('<div style="overflow: hidden; width:' + avatarWidth + 'px; height:' + avatarHeight + 'px;">' +
-					'<img alt="' + nameAObj.html() + '" src="' + gamesArr[gameIndex][nameAObj.html()].fakeAvatar + '"></img>' +
+				.html('<div style="position: relative; overflow: hidden; width:' + avatarWidth + 'px; height:' + avatarHeight + 'px;">' +
+					'<img style="position: absolute; display: block;" alt="' + nameAObj.html() + '" src="' + gamesArr[gameIndex][nameAObj.html()].fakeAvatar + '"></img>' +
 					'</div>')
 				.prependTo(parentObj);
 
@@ -62,56 +60,40 @@ jQuery(document).ready(function()
 			var shiftTop;
 			var shiftLeft;
 
+			// Calculate new height and width
 			var width = $img.width();
 			var height = $img.height();
-			var pow;
 
-			// Comment with picture 903x1024 example
-			if ((height / avatarHeight) > (width / avatarWidth)) // 1024/128 > 903/128
-			{ 
-				if (avatarHeight - height > 0) 
-				{
-					pow = 1
-				} // 128 - 1024 < 0
-				else 
-				{
-					pow = -1
-				} // pow = -1
-				newWidth = avatarWidth; // = 128
-				newHeight = height * Math.pow(width / avatarWidth, pow); // = 1024 * (903/128)^(-1) = 1024 * 128 / 903 = 145,15171650055370985603543743079
-				newHeight = Math.ceil(newHeight); // 146
-				shiftLeft = 0;
-				shiftTop = -(newHeight - avatarHeight) / 2; // = - ( 145 - 128 ) / 2 = -8,5
-				shiftTop = Math.floor(shiftTop); // -8
+			if (height > width) 
+			{
+				newWidth = avatarWidth + 'px';
+				newHeight = 'auto';
 			} 
 			else 
 			{
-				if (avatarWidth - width > 0) 
-				{
-					pow = 1
-				} 
-				else 
-				{
-					pow = -1
-				}
-				newHeight = avatarHeight;
-				newWidth = width * Math.pow(height / avatarHeight, pow);
-				newWidth = Math.ceil(newWidth);
-				shiftTop = 0;
-				shiftLeft = -(newWidth - avatarWidth) / 2;
-				shiftLeft = Math.floor(shiftLeft);
+				newWidth = 'auto';
+				newHeight = avatarHeight + 'px';
 			}
 
-			$img.attr("height", newHeight)
-				.attr("width", newWidth)
-				.attr("style", 'margin-top: ' + shiftTop + 'px; margin-left: ' + shiftLeft + 'px;');
+			$img.css("height", newHeight)
+				.css("width", newWidth);
+
+			// Calculate margin top and left to centerlize image         
+			width = $img.width(); // image have already new width - get it
+			height = $img.height(); // image have already new height - get it
+
+			shiftTop = (avatarHeight - height) / 2 + 'px';
+			shiftLeft = (avatarWidth - width) / 2 + 'px';
+
+			$img.css("margin-top", shiftTop)
+				.css("margin-left", shiftLeft);
 
 			gamesArr[gameIndex][nameAObj.html()].fakeAvatarObj = newNameAObj;
 		} 
 		else 
 		{
 			// Else we have already met this avatar earlier, so we can clone it and make a substitution
-            
+
 			// No need to recalculate image size again ;)
 			gamesArr[gameIndex][nameAObj.html()].fakeAvatarObj
 				.clone()
@@ -121,6 +103,9 @@ jQuery(document).ready(function()
 		// Change name
 		nameAObj.html(gamesArr[gameIndex][nameAObj.html()].fakeNickname);
 	}
+
+
+	// MAIN()
 
 	var currentTitle = $("#page-body").children("h2").children("a").html();
 
